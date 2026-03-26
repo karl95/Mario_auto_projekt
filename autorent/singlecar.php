ade48b5493630c27a08c93fe0ae070c3f82286a6
karl95 <karlkessel79@gmail.com>
2026-03-26 15:37:14 +0200

    Add files via upload

---
+++ autorent/singlecar.php
@@ -0,0 +1,100 @@
+<?php include("config.php"); ?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Single car</title>
+  </head>
+  <body>
+    <h1>Single car</h1>
+<!-- menüü -->
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Dropdown
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                    </li>
+                </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+<!-- /menüü -->
+
+<?php
+        $paring = "SELECT * FROM cars WHERE id=25";
+        $valjund = mysqli_query($yhendus, $paring);
+        $rida = mysqli_fetch_assoc($valjund);
+
+
+        //while($rida = mysqli_fetch_assoc($valjund)){
+        //var_dump($rida);
+      ?>
+
+<!-- sisu -->
+    <div class="container mt-4">
+        <div class="row">
+            <div class="col-sm-6">
+                <img src="https://loremflickr.com/400/250/<?php echo $rida ['mark']; ?>" class="card-img-top" alt="audi">
+            </div>
+            <div class="col-sm-6">
+                <h2><?php echo $rida ['mark']." ".$rida ['model']; ?></h2>
+                <p>2010<?php echo $rida ['year']; ?></p>
+                <p>MOotor: <?php echo $rida ['engine']; ?></p>
+                <p>Kütus: <?php echo $rida ['fuel']; ?></p>
+                <p>Käigukast: <?php echo $rida ['transmission']; ?></p>
+                <p>Kohad: <?php echo $rida ['kohad']; ?></p>
+                <p>Hind: <?php echo $rida ['price']; ?>€/päev</p>
+                <a href="#" class="btn btn-dark w-100">Rendi auto</a>
+            </div>
+        </div>
+    </div>
+<!-- /sisu -->
+
+<!--Andmebaas:   
+    https:\\localhost/phpmyadmin
+    kasutajanimi: karlk ja parool: Passw0rd
+-->
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
