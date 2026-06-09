0b288a95699a8d77eb78e245de35abdd724ac1ec
karl95 <karlkessel79@gmail.com>
2026-06-09 19:53:01 +0000

    Fix: Kogu veebirakenduse kood uuendatud ja Dockeri tugi lisatud

--- config.php
+++ config.php
@@ -1,18 +1,15 @@
 <?php
 // *** Protseduuriline *** //
-// Sinu andmed
-$db_server = 'localhost';
-$db_andmebaas = 'auto_rent';
-$db_kasutaja = 'karlk';
-$db_salasona = 'Passw0rd';
-
+$db_server = 'db';           // Ühtib docker-compose.yml teenuse nimega
+$db_andmebaas = 'cr_rent';    // Ühtib MYSQL_DATABASE väärtusega
+$db_kasutaja = 'autorent';    // Ühtib MYSQL_USER väärtusega
+$db_salasona = 'autorent';    // Ühtib MYSQL_PASSWORD väärtusega
 
 // Ühendus andmebaasiga
 $yhendus = mysqli_connect($db_server, $db_kasutaja, $db_salasona, $db_andmebaas);
 
-
 // Ühenduse kontroll
 if (!$yhendus) {
-    die('Ei saa ühendust andmebaasiga');
+    die('Ei saa ühendust andmebaasiga: ' . mysqli_connect_error());
 }
 ?>
